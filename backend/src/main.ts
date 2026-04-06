4d97219ab69717e57e68e3ddb41c70dc36c911ca
AbdullahAnsari-03 <223abdullah0007@dbit.in>
2026-04-06 20:49:02 +0530

    Frontend start

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  console.log("APP STARTED 🚀"); // 👈 ADD THIS LINE
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
+
+  await app.listen(3000);
+  console.log('Server is running on http://localhost:3000');
 }
 bootstrap();
