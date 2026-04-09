83a6d15c55f5c9045ddb5bfc4c39e5bae2726605
AbdullahAnsari-03 <223abdullah0007@dbit.in>
2026-04-10 00:47:31 +0530

    fix

--- frontend/lib/apollo-client.ts
+++ frontend/lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:3000/graphql',
+    uri: 'https://slooze-backend-z2iq.onrender.com/graphql',
   }),
   cache: new InMemoryCache(),
 });
