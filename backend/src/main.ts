5816d0cf1985cb7cf433edf385db5a57023acc6a
AbdullahAnsari-03 <223abdullah0007@dbit.in>
2026-04-10 00:26:40 +0530

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*', 
     credentials: true,
   });
 
-  await app.listen(3000);
-  console.log('Server is running on http://localhost:3000');
+  const PORT = process.env.PORT || 3000;
+
+  await app.listen(PORT);
+  console.log(`Server is running on port ${PORT}`);
 }
 bootstrap();
